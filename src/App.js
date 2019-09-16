c2d3e90494e8bc132e320ec5b84b688896ccce97
Mildred Awuor <otieno.awuor.m@gmail.com>
2019-09-16 15:46:45 +0300

    MVP completed

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 import {Route} from "react-router-dom";
 
+
+
 const App = () => (
   <div>
     <Navigation />
-    <Route exact path="/"  components={Home}></Route>
-    <Route path="/About" components={About}></Route>
-    <Route path="/Contact" components={Contact}></Route>
+    <Route exact path="/"  component={Home}></Route>
+    <Route path="/About" component={About}></Route>
+    <Route path="/Contact" component={Contact}></Route>
   </div>
 );
 
